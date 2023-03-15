5f498f74d5e123befec0e68c92efcc11721a49fc
Sandip <sandipramanik07@gmail.com>
2023-03-15 09:38:42 +0530

    feat: add delete user functionality and generic exception handling

--- src/main/java/dev/thecodexhub/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/dev/thecodexhub/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,7 @@ package dev.thecodexhub.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,15 +36,17 @@ public class UserResource {
     return user;
   }
 
+  @DeleteMapping("/users/{id}")
+  public void deleteUser(@PathVariable int id) {
+    service.deleteById(id);
+  }
+
   @PostMapping("/users")
   public ResponseEntity<User> createUser(@RequestBody User user) {
     User savedUser = service.save(user);
 
-    URI location = ServletUriComponentsBuilder
-      .fromCurrentRequest()
-      .path("/{id}")
-      .buildAndExpand(savedUser.getId())
-      .toUri();
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(savedUser.getId()).toUri();
     return ResponseEntity.created(location).build();
   }
 }
